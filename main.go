59263e986cee14f9319ed17f0df89b861a80b56a
Pham Viet Hoa <152608093+phamhoa2416@users.noreply.github.com.>
2025-02-23 09:11:20 +0700

    feat: create parent context for the application

--- main.go
+++ main.go
@@ -3,15 +3,22 @@ package main
 import (
 	"context"
 	"log"
+	"os"
+	"os/signal"
+
 	"microservices/application"
 )
 
 func main() {
 	app := application.New()
 
-	err := app.Init(context.TODO())
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	err := app.Init(ctx)
 
 	if err != nil {
+
 		log.Fatalf("failed to start server: %v", err)
 	}
 }
